192d4cb1eddb4ab197cd899374c665f170ec016a
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-14 21:50:43 +0600

    add

--- BackendClass1/server.js
+++ BackendClass1/server.js
@@ -6,6 +6,13 @@ app.listen(3000, () => {
   console.log("Server Started at port no. 3000");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
+
+app.post("/api/cars", (request, response) => {
+  const { name, brand } = request.body;
+  console.log(name);
+  console.log(brand);
+  response.send("Car Submitted Successfully");
 });
