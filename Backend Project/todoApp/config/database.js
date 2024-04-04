7ce45dc8fb3f813e983a1365ac33e1e5ce4eeabe
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-04 14:30:48 +0600

    add

---
+++ Backend Project/todoApp/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const dbConnect = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+      console.log("Database connection i successful");
+    })
+    .catch((error) => {
+      console.log("Error in database connection");
+      console.error(error.message);
+      process.exit(1);
+    });
+};
+
+module.exports = dbConnect;
