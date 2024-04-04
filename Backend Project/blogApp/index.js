3bacc7a23dc24607959a03942ccecfdcc4e5f56d
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-04 22:06:42 +0600

    add

--- Backend Project/blogApp/index.js
+++ Backend Project/blogApp/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("App is running successfully");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+// !middleware
+app.use(express.json());
+
+const blog = require("./routes/blog");
+// !mount
+app.use("/api/v1", blog);
+
+const connectWithDb = require("./config/database");
+connectWithDb();
+
+// !start the server
+app.listen(PORT, () => {
+  console.log(`App is started at Port no. ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-  res.send(`<h1>This is homepage</h1>`);
+  res.send(`<h1>This is my homepage</h1>`);
 });
