e209b51c0eca6c9e9ed4316d0781c791d1bd7924
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-05 10:55:07 +0600

    add

--- Backend Project/blogApp/routes/blog.js
+++ Backend Project/blogApp/routes/blog.js
@@ -4,11 +4,14 @@ const router = express.Router();
 // !import controller
 const { createComment } = require("../controllers/CommentController");
 const { createPost, getAllPosts } = require("../controllers/PostController");
+const { likePost, unlikePost } = require("../controllers/LikeController");
 
 // !mapping create
 router.post("/comments/create", createComment);
 router.post("/posts/create", createPost);
 router.get("/posts", getAllPosts);
+router.post("/likes/like", likePost);
+router.post("/likes/unlike", unlikePost);
 
 // !export
 module.exports = router;
