c321b7a48588fba3d28d149da38b914030263eee
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-06 14:47:04 +0600

    add

--- Backend Project/authApp/middlewares/middleAuth.js
+++ Backend Project/authApp/middlewares/middleAuth.js
@@ -1,12 +1,11 @@
 // auth , isStudent , isAdmin
-
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 exports.auth = (req, res, next) => {
   try {
     // extract JWT token
-    const { token } = req.body.token || req.cookies.token;
+    const { token } = req.body.token;
     if (!token) {
       return res.status(401).json({
         success: false,
