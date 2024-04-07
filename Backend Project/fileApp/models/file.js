9070b182328068baacdfac629fae0113abc443f7
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-07 14:08:13 +0600

    add

--- Backend Project/fileApp/models/file.js
+++ Backend Project/fileApp/models/file.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -14,6 +15,35 @@ const fileSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+// post middleware
+
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("DOC: ", doc);
+    // transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // send mail
+    let info = await transporter.sendMail({
+      from: `Kabir`,
+      to: doc.email,
+      subject: "New File Uploaded on Cloudinary",
+      html: `<h2>Hello World</h2>
+              <p>File Uploaded</p> 
+              View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a>`,
+    });
+    console.log("Info: ", info);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = mongoose.model("File", fileSchema);
